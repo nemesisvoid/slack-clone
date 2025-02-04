1b110e46482a8ee2d1f29b63a1af5ebb1f7f6f13
nemesisvoid <samsonajibade40@gmail.com>
2025-02-04 18:30:22 +0100

    minor changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
 'use client';
 
-import { UserButton } from '@/features/auth/components/user-button';
 import { useCreateWorkspaceModal } from '@/features/workspaces/store/use-create-workspace-modal';
 import { useGetWorkspaces } from '@/features/workspaces/api/use-get-workspaces';
 import { useEffect, useMemo } from 'react';
 import { useRouter } from 'next/navigation';
+import { Loader } from 'lucide-react';
 
 export default function Home() {
   const { data, isLoading } = useGetWorkspaces();
@@ -25,8 +25,8 @@ export default function Home() {
     }
   }, [workspaceId, isLoading, open, setOpen, router]);
   return (
-    <div className='text-rose-500'>
-      <UserButton />
+    <div className='h-full flex items-center justify-center'>
+      <Loader className='size-10 animate-spin text-muted-foreground' />
     </div>
   );
 }
